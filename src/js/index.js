325ba6f00ae1405ac88f91e1b9c3a0fa0bc05222
浅井 貴之 <asai-ta@gnavi.co.jp>
2018-10-10 11:35:55 +0900

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
+const logs = []
+
+const logger = v => {
+  logs.push(v)
+  document.getElementById('js-log').innerHTML = logs.join('<br>')
+}
+
 window.addEventListener('load', () => {
   liff.init(
-    () => console.log('init success'),
-    () => console.log('init error'),
+    () => logger('init success'),
+    () => logger('init error'),
   )
 })
