9a9b401dc8974509b8b5ae36b4efc445f52ad27d
浅井 貴之 <asai-ta@gnavi.co.jp>
2018-10-10 14:19:15 +0900

    setLink

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '@babel/polyfill'
+import forEach from 'lodash/forEach'
 
 const logs = []
 
@@ -16,8 +17,21 @@ async function initialize() {
   })
 }
 
+const setLink = () => {
+  const triggers = document.querySelectorAll('.js-link')
+  forEach(triggers, elm => {
+    elm.addEventListener('click', () => {
+      liff.openWindow({
+        url: elm.getAttribute('data-url'),
+        external: elm.getAttribute('data-window') === 'out',
+      })
+    })
+  })
+}
+
 window.addEventListener('load', async () => {
   logger('liff initial start')
   await initialize()
   logger('liff initial end')
+  setLink()
 })
